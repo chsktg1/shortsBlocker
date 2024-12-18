ebd0a53cc8f8829db444953a5bc48d84dee64a99
chskTg <beingmsd77@gmail.com>
2024-12-18 16:07:55 -0500

    Initial Commit

---
+++ shortsBlockerTests/shortsBlockerTests.swift
@@ -0,0 +1,17 @@
+//
+//  shortsBlockerTests.swift
+//  shortsBlockerTests
+//
+//  Created by Sumanth Kumar on 12/18/24.
+//
+
+import Testing
+@testable import shortsBlocker
+
+struct shortsBlockerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
